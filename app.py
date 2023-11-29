b346b5b98eacca2e1dc5b4ddc98fe1440337c506
MichelRibeiro48 <michel-amoral@hotmail.com>
2023-11-29 16:25:40 -0300

    adding binary tree numbers test

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from collections import deque
+import functools
 from selenium import webdriver
 
 # Usada para gerenciar automaticamente os drivers do navegador
@@ -10,8 +12,89 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
-# Instanciando o driver do Edge
+#Preparando Função para testar a arvore binaria]
 
+def OrdenarListaArvore(numeros): 
+  lista = numeros.split('(')
+  lista = [s.strip(')') for s in lista]
+  lista.pop(0)
+  lista = [int(s) for s in lista]
+  return lista
+# novalista = OrdenarListaArvore('(87(48(37))(98))')
+
+def compararListas(lista1, numeros2): 
+  lista2 = numeros2.split(', ')
+  lista2 = [int(s) for s in lista2]
+  if functools.reduce(lambda x, y : x and y, map(lambda p, q: p == q,lista2,lista1), True):
+    return True
+  else:
+    return False
+class Node:
+   def __init__(self, data):
+      self.left = None
+      self.right = None
+      self.data = data
+# Insert Node
+   def insert(self, data):
+      if self.data:
+         if data < self.data:
+            if self.left is None:
+               self.left = Node(data)
+            else:
+               self.left.insert(data)
+         elif data > self.data:
+            if self.right is None:
+               self.right = Node(data)
+            else:
+               self.right.insert(data)
+         else:
+            self.data = data
+   def PrintTree(self):
+      if self.left:
+         self.left.PrintTree()
+      print( self.data),
+      if self.right:
+         self.right.PrintTree()
+   def PreorderTraversal(self, root):
+      res = []
+      if root:
+         res.append(root.data)
+         res = res + self.PreorderTraversal(root.left)
+         res = res + self.PreorderTraversal(root.right)
+      return res
+   def PostorderTraversal(self, root):
+    res = []
+    if root:
+      res = self.PostorderTraversal(root.left)
+      res = res + self.PostorderTraversal(root.right)
+      res.append(root.data)
+    return res
+   def inOrderTraversal(self, root):
+      res = []
+      if root:
+         res = self.inOrderTraversal(root.left)
+         res.append(root.data)
+         res = res + self.inOrderTraversal(root.right)
+      return res
+   def BFSTraversal(self, root):
+        if root is None:
+            return []
+
+        res = []
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            current_node = queue.popleft()
+            res.append(current_node.data)
+
+            if current_node.left:
+                queue.append(current_node.left)
+            if current_node.right:
+                queue.append(current_node.right)
+
+        return res
+# Instanciando o driver do Edge
 driver = webdriver.Edge()
 
 #O método driver.get navegará para uma página fornecida pela URL.
@@ -60,6 +143,18 @@ titlePreOrderHistory = driver.find_element(by=By.XPATH, value='//*[@id="__next"]
 assert titlePreOrderHistory == 'Pre order'
 time.sleep(3)
 
+#Checando se a arvore foi ordenada corretamente em Pre Order
+treeInString = driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div/div[2]/div[3]/ul/li[1]/p').text
+preOrderInString = driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div/div[2]/div[3]/ul/li[3]/p').text
+novalista = OrdenarListaArvore(treeInString)
+root = Node(novalista[0])
+for i in range(len(novalista)):
+   root.insert(novalista[i])
+assert compararListas(root.PreorderTraversal(root), preOrderInString) == True
+print("ordenação feita pelo python",root.PreorderTraversal(root))
+print("ordenação feita pelo site",preOrderInString)
+time.sleep(3)
+
 #Clicando no botão InOrder
 InTreeButton = driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div/div[2]/div[1]/button[3]')
 InTreeButton.click()
@@ -68,6 +163,16 @@ time.sleep(3)
 #Checando se aparece In Order no Historico
 titleInOrderHistory = driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div/div[2]/div[3]/ul/li[4]/h3').text
 assert titleInOrderHistory == 'In order'
+time.sleep(3)
+
+#Checando se a arvore foi ordenada corretamente em In Order
+InOrderInString = driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div/div[2]/div[3]/ul/li[4]/p').text
+root = Node(novalista[0])
+for i in range(len(novalista)):
+   root.insert(novalista[i])
+assert compararListas(root.inOrderTraversal(root), InOrderInString) == True
+print("ordenação feita pelo python", root.inOrderTraversal(root))
+print("ordenação feita pelo site", InOrderInString)
 time.sleep(3)
 
 #Clicando no botão PostOrder
@@ -80,6 +185,16 @@ titlePostOrderHistory = driver.find_element(by=By.XPATH, value='//*[@id="__next"
 assert titlePostOrderHistory == 'Post order'
 time.sleep(3)
 
+#Checando se a arvore foi ordenada corretamente em Post Order
+postOrderInString = driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div/div[2]/div[3]/ul/li[5]/p').text
+root = Node(novalista[0])
+for i in range(len(novalista)):
+   root.insert(novalista[i])
+assert compararListas(root.PostorderTraversal(root), postOrderInString) == True
+print("ordenação feita pelo python",root.PostorderTraversal(root))
+print("ordenação feita pelo site",postOrderInString)
+time.sleep(3)
+
 #Clicando no botão Breadth-first search
 BreadthFirstTreeButton = driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div/div[2]/div[1]/button[5]')
 BreadthFirstTreeButton.click()
@@ -88,6 +203,16 @@ time.sleep(3)
 #Checando se aparece Breadth-first search no Historico
 titleBreadthFirstHistory = driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div/div[2]/div[3]/ul/li[6]/h3').text
 assert titleBreadthFirstHistory == 'Breadth-first search'
+time.sleep(3)
+
+#Checando se a arvore foi ordenada corretamente em BFS
+BFSInString = driver.find_element(by=By.XPATH, value='//*[@id="__next"]/div/div[2]/div[3]/ul/li[6]/p').text
+root = Node(novalista[0])
+for i in range(len(novalista)):
+   root.insert(novalista[i])
+assert compararListas(root.BFSTraversal(root), BFSInString) == True
+print("ordenação feita pelo python",root.BFSTraversal(root))
+print("ordenação feita pelo site",BFSInString)
 time.sleep(3)
 
 #Clicando no botão Staff
